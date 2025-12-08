b1bbe5062aac313b72e3ea57f163d641fb992e47
Krisztian Bito <kbito@me.com>
2025-12-08 11:59:07 +0100

    Initial Commit

---
+++ wordformat/wordformatApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordformatApp.swift
+//  wordformat
+//
+//  Created by Krisztian Bito on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct wordformatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wordformatTests/wordformatTests.swift
@@ -0,0 +1,17 @@
+//
+//  wordformatTests.swift
+//  wordformatTests
+//
+//  Created by Krisztian Bito on 08.12.2025.
+//
+
+import Testing
+@testable import wordformat
+
+struct wordformatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
